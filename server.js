4ea5b5ba0e45dfca7a075ccc878c97b51ea7a391
kittenz <windlolz55@gmail.com>
2026-07-30 10:35:48 +0800

    做網站

---
+++ server.js
@@ -0,0 +1,6 @@
+﻿const { PeerServer } = require('peer');
+
+const port = process.env.PORT || 9000;
+const peerServer = PeerServer({ port: port, path: '/' });
+
+console.log('PeerJS server running on port ' + port);
